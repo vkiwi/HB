993804accdf27ecdc1ef826c95ac20b4263c0043
Viktor <viktor.kochegarov@gmail.com>
2020-09-09 23:05:45 +0300

    докрутки слайдера, формы, карты, + фиксированный хедер

--- common.js
+++ common.js
@@ -44,7 +44,57 @@ $.validator.addMethod('js-input-phone', function(value, element){
 	
 	$('.js-form').on('formSubmit', function() {
 		alert('Форма отправлена');
-	})
+	});
+
+// $('.form__select').select2({
+// 	appearance: none
+// });
+
+ymaps.ready(init);
+function init(){
+    var map = document.querySelector('#map');
+    var center = map.dataset.center.split(',');
+    center[0] = +center[0];
+    center[1] = +center[1];
+
+    var mark = map.dataset.placemark.split(',');
+    mark[0] = +mark[0];
+    mark[1] = +mark[1];
+
+    var myMap = new ymaps.Map("map", {
+        center: center,
+        zoom: 15,
+        controls: []
+    });
+
+    var placemarkContent = ymaps.templateLayoutFactory.createClass(
+        '<div class="placemark-content" style="padding: 5px 10px; width: 120px; background-color: rgba(255,255,255,0.8);">$[properties.iconContent]</div>'
+    );
+
+    var placemark = new ymaps.Placemark(mark, {
+        iconContent: 'улица Ильинка, 4'
+    }, {
+        iconLayout: 'default#imageWithContent',
+        iconImageHref: 'images/map.png',
+        iconImageSize: [60, 60],
+        iconImageOffset: [-20, -20],
+        iconContentOffset: [45, 10],
+        iconContentLayout: placemarkContent
+    });
+
+    myMap.geoObjects.add(placemark);
+}
+
+
+$(window).scroll(function(){
+    console.log($(this).scrollTop());
+    if($(this).scrollTop()>900) {
+       $(".wrapper2").show();
+      }
+        else {$(".wrapper2").hide() ;
+      }
+    });
+
 
 
 });
